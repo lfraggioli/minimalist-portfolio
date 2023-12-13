ef20c06f58022bb4afca99acf76bc3ec22280098
Lucas Fraggioli <fraggioli.lucas@gmail.com>
2023-12-13 18:39:53 -0300

    stack added

--- src/components/Stack.jsx
+++ src/components/Stack.jsx
@@ -3,7 +3,56 @@ import React from "react";
 function Stack() {
   return (
     <>
-      <div className="flex items-center justify-center flex-col text-center pt-20 pb-5 text-slate-100"></div>
+      <div className="flex items-center gap-2 justify-center flex-row text-center pt-20 pb-5 text-slate-100">
+        <img
+          src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"
+          alt="JavaScript"
+          width="64"
+          height="64"
+        />
+        <img
+          src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg"
+          alt="React"
+          width="64"
+          height="64"
+        />
+        <img
+          src="https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg"
+          alt="Java"
+          width="64"
+          height="64"
+        />
+
+        <img
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg"
+          width="64"
+          height="64"
+        />
+        <img
+          src="https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg"
+          alt="Redux"
+          width="64"
+          height="64"
+        />
+
+        <img
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-plain.svg"
+          width="64"
+          height="64"
+        />
+        <img
+          src="https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg"
+          alt="PostgreSQL"
+          width="64"
+          height="64"
+        />
+
+        <img
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg"
+          width="64"
+          height="64"
+        />
+      </div>
     </>
   );
 }
